cf02090d17014b2facc4022fb7e5770bbb7f5b3c
mike <mike.hsn.work@gmail.com>
2023-02-09 17:47:59 +0200

    fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Mikes', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Mikes/i);
+  //console.log(">>>", linkElement)
   expect(linkElement).toBeInTheDocument();
+});
+
+test('sanity', () => {
+  render(<App />);
+  expect(1).toEqual(1);
 });
