8a1b131126b588a41bee52a7da55f28b6f73d5d5
mike <mike.hsn.work@gmail.com>
2023-02-10 10:29:58 +0200

    fix: update title to fit requirment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Miikes Page !
+          Mikes Page !
         </p>
         <Counter />
         <p>Works !</p>
